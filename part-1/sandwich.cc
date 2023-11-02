9102b76a6ffbf62bb60ba6a38c9f5cebce168b85
kevindo05 <143019516+kevindo05@users.noreply.github.com>
2023-11-02 04:32:28 +0000

    fixed bugs

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -9,19 +9,17 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments(argv, argv + argc);
-  if (arguments.size() < 4) {
+  if (arguments.size() != 4) {
     std::cout << "Error: You must supply three arguments.\n";
     return 1;
-  } else if (arguments.size() > 4) {
-    std::cout << "Error: You must supply three arguments.\n";
-    return 1;
-  }
+  } else {
+  };
+
+  std::string protein{arguments.at(1)};
+  std::string bread{arguments.at(2)};
+  std::string condiment{arguments.at(3)};
+  std::cout << "Your order: A " << protein << " sandwich on " << bread
+            << " with " << condiment << ".\n";
+
+  return 0;
 }
-
-std::string protein{arguments.at(1)};
-std::string bread{arguments.at(2)};
-std::string condiment{arguments.at(3)};
-std::cout << "A " << protein << " sandwich on " << bread << " with "
-          << condiment << ".\n";
-
-return 0;
